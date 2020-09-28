dccafeb564d1b8d3fe63a31e2a1defc8541a968d
Darshan <darshan.s@prixgen.com>
2020-09-28 13:51:36 +0530

    restored

--- mrpindent/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import indent
-from . import stock_indent
-from . import stock_move
-from . import mrp_production
-from . import res_config_settings
-#from . import stock_picking
--- purchase_request/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import purchase_request
-from . import mo_request
